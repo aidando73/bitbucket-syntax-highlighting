651690a3a1c67926333372601c5520ed0a7ff351
Aidan Do <aidan.do@nqry.com.au>
2022-09-24 17:26:49 +0930

    format document with prettier

--- content-script.js
+++ content-script.js
@@ -1,28 +1,26 @@
-const _allCodeLinesCssSelector = '[data-qa=code-line] pre > span:last-child'
+const _allCodeLinesCssSelector = "[data-qa=code-line] pre > span:last-child";
 
-const allDiffsObserver = new MutationObserver(mutations => {
-	for (const mutation of mutations) {
-			// Only act when new nodes are added
-			if (
-					!(mutation.type === 'childList' && mutation.addedNodes.length > 0)
-			) {
-					continue
-			}
+const allDiffsObserver = new MutationObserver((mutations) => {
+  for (const mutation of mutations) {
+    // Only act when new nodes are added
+    if (!(mutation.type === "childList" && mutation.addedNodes.length > 0)) {
+      continue;
+    }
 
-			// For each line in the diff block
-			mutation.target
-					.querySelectorAll(_allCodeLinesCssSelector)
-					.forEach(elem => {
-							highlightDiff(elem)
-					})
-	}
-})
+    // For each line in the diff block
+    mutation.target
+      .querySelectorAll(_allCodeLinesCssSelector)
+      .forEach((elem) => {
+        highlightDiff(elem);
+      });
+  }
+});
 
 waitForElement('section[aria-label="Diffs"]').then((diffSection) => {
-	allDiffsObserver.observe(diffSection, {
-		childList: true,
-		subtree: true,
-	});
+  allDiffsObserver.observe(diffSection, {
+    childList: true,
+    subtree: true,
+  });
 });
 
 function waitForElement(selector) {
@@ -46,22 +44,18 @@ function waitForElement(selector) {
 }
 
 function highlightDiff(codeLine) {
-	console.log(codeLine);
-	// Try to get the extension of the file
-	const article = codeLine.closest(
-		'article[data-qa="pr-diff-file-styles"]'
-	);
-	const ariaAttribute = article.getAttribute("aria-label");
+  console.log(codeLine);
+  // Try to get the extension of the file
+  const article = codeLine.closest('article[data-qa="pr-diff-file-styles"]');
+  const ariaAttribute = article.getAttribute("aria-label");
 
-	const extension = getExtension(ariaAttribute);
+  const extension = getExtension(ariaAttribute);
 
-	article
-	.querySelectorAll(_allCodeLinesCssSelector)
-	.forEach((node) => {
-		node.classList.add("language-java");
-		// node.classList.add("__rbb_syntax_highlight")
-		Prism.highlightElement(node);
-	});
+  article.querySelectorAll(_allCodeLinesCssSelector).forEach((node) => {
+    node.classList.add("language-java");
+    // node.classList.add("__rbb_syntax_highlight")
+    Prism.highlightElement(node);
+  });
 }
 
 function getExtension(filepath) {
