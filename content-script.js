0d1ccb1b9e247370ce75320a31c20f67be8d21e5
Aidan Do <43259657+REslim30@users.noreply.github.com>
2022-09-24 17:44:58 +0930

    Syntax highlight all java diffs (#4)
    * Syntax highlight all java diffs
    
    * use constant over hardcoding
    
    * format document with prettier
    
    * Use a more fluent form of looping
    
    * Remove old console.log
    
    * Remove console.log
    
    * indent using spaces
    
    Co-authored-by: Aidan Do <aidan.do@nqry.com.au>

--- content-script.js
+++ content-script.js
@@ -1,59 +1,59 @@
-// new SelectorObserver(
-// 	document.body,
-// 	'section[aria-label="Diffs"]',
-// 	function() {
-// 		// syntaxHighlightNewUI(this, config.syntaxHighlightTheme)
-// 		console.log("Hello within selection observer!!!")
-// 	}
-// )
-console.log("Hello World from Bitbucket DIFF!")
+const _allCodeLinesCssSelector = "[data-qa=code-line] pre > span:last-child";
 
-waitForElement('section[aria-label="Diffs"]')
-	.then(diffSection => {
-		console.log("diffSection", diffSection);
-		setTimeout(() => {
-			const allCodeLines = diffSection.querySelectorAll("[data-qa=code-line] pre > span:last-child");
-			console.log(allCodeLines);
-			allCodeLines.forEach(codeLine => {
-				console.log(codeLine);
-				// Try to get the extension of the file
-				const article = codeLine.closest(
-					'article[data-qa="pr-diff-file-styles"]'
-				)
-				const ariaAttribute = article.getAttribute('aria-label')
+const allDiffsObserver = new MutationObserver((mutations) => {
+  mutations
+    .filter((mutation) => mutation.type === "childList")
+    .filter((mutation) => mutation.addedNodes.length > 0)
+    .forEach((mutation) => {
+      mutation.target
+        .querySelectorAll(_allCodeLinesCssSelector)
+        .forEach((elem) => {
+          highlightDiff(elem);
+        });
+    });
+});
 
-				const extension = getExtension(ariaAttribute)
-
-				const nodes = article.querySelectorAll("[data-qa=code-line] pre > span:last-child");
-				nodes.forEach(node => {
-					node.classList.add("language-java")
-					// node.classList.add("__rbb_syntax_highlight")
-					Prism.highlightElement(node)
-				})
-			});
-		}, 2000);
-	});
+waitForElement('section[aria-label="Diffs"]').then((diffSection) => {
+  allDiffsObserver.observe(diffSection, {
+    childList: true,
+    subtree: true,
+  });
+});
 
 function waitForElement(selector) {
-	return new Promise(resolve => {
-			if (document.querySelector(selector)) {
-					return resolve(document.querySelector(selector));
-			}
+  return new Promise((resolve) => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
 
-			const observer = new MutationObserver(mutations => {
-					if (document.querySelector(selector)) {
-							resolve(document.querySelector(selector));
-							observer.disconnect();
-					}
-			});
+    const observer = new MutationObserver((mutations) => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
+    });
 
-			observer.observe(document.body, {
-					childList: true,
-					subtree: true
-			});
-	});
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
+
+function highlightDiff(codeLine) {
+  // Try to get the extension of the file
+  const article = codeLine.closest('article[data-qa="pr-diff-file-styles"]');
+  const ariaAttribute = article.getAttribute("aria-label");
+
+  const extension = getExtension(ariaAttribute);
+
+  article.querySelectorAll(_allCodeLinesCssSelector).forEach((node) => {
+    node.classList.add("language-java");
+    // node.classList.add("__rbb_syntax_highlight")
+    Prism.highlightElement(node);
+  });
 }
 
 function getExtension(filepath) {
-	return `.${filepath.slice(((filepath.lastIndexOf('.') - 1) >>> 0) + 2)}`
+  return `.${filepath.slice(((filepath.lastIndexOf(".") - 1) >>> 0) + 2)}`;
 }
