b50f3c1b71cfa84d3ef60275b846b66f436a0fb0
Aidan Do <43259657+REslim30@users.noreply.github.com>
2022-10-13 21:04:29 +1030

    chore: Added in basic jest tests (#32)
    Co-authored-by: Aidan Do <aidan.do@nqry.com.au>

---
+++ static/wait-for-element.js
@@ -0,0 +1,22 @@
+function waitForElement(selector) {
+    return new Promise((resolve) => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver((mutations) => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true,
+        });
+    });
+}
+
+// This is to allow for testing
+global.waitForElement = waitForElement;
